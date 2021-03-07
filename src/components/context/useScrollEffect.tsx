5aa9a5dc721d07f0cf46b4213298e951a2bc5f1c
okatechnology <okatechnology2020@gmail.com>
2021-03-07 11:04:35 +0900

    feature: useScrollEffect

---
+++ src/components/context/useScrollEffect.tsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useEffect, useState, FC } from 'react';
+
+interface ScrollEffectProps {
+  scrollY: number;
+}
+
+export const useScrollEffect = () => useContext(useScrollEffect.context);
+useScrollEffect.context = createContext<ScrollEffectProps>(undefined as any);
+
+export const ScrollEffectProvier: FC = ({ children }) => {
+  const [scrollY, setScrollY] = useState<number>(0);
+
+  useEffect(() => {
+    const scrollEvent = () => {
+      setScrollY(window.pageYOffset)
+    };
+    window.addEventListener('scroll', scrollEvent);
+
+    return () => {
+      window.removeEventListener('scroll', scrollEvent);
+    };
+  }, []);
+
+  return (
+    <useScrollEffect.context.Provider value={{ scrollY }}>
+      {children}
+    </useScrollEffect.context.Provider>
+  );
+};
