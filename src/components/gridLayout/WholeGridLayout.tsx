245d9c9673d7a755d61a3627330ac94484b81bfc
okatechnology <okatechnology2020@gmail.com>
2021-03-10 14:08:54 +0900

    change: whole styles and how it works

--- src/components/gridLayout/WholeGridLayout.tsx
+++ src/components/gridLayout/WholeGridLayout.tsx
@@ -3,6 +3,8 @@ import { ReactNode, VFC } from 'react';
 interface WholeGridLayoutProps {
   mainVisual: ReactNode;
   contents: ReactNode;
+  contentsVisiable: boolean;
+  visiableTransitionEnd: () => void;
 }
 export const WholeGridLayout: VFC<WholeGridLayoutProps> = (props) => {
   return <WholeGridLayoutPresentational {...props} />;
@@ -12,9 +14,20 @@ interface WholeGridLayoutPresentationalProps extends WholeGridLayoutProps {}
 const WholeGridLayoutPresentational: VFC<WholeGridLayoutPresentationalProps> = ({
   mainVisual,
   contents,
+  contentsVisiable,
+  visiableTransitionEnd,
 }) => (
   <div className="pc:grid grid-cols-2">
-    {mainVisual}
+    <div className="z-50">
+      <div
+        className={`fixed top-0 left-0 bottom-0 ${
+          contentsVisiable ? 'right-1/2' : 'right-0'
+        } transition-directions duration-700`}
+        onTransitionEnd={visiableTransitionEnd}
+      >
+        {mainVisual}
+      </div>
+    </div>
     <div>{contents}</div>
   </div>
 );
--- src/components/templates/IndexTemplate.tsx
+++ src/components/templates/IndexTemplate.tsx
@@ -1,16 +1,31 @@
-import React, { useMemo, useRef, VFC } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  VFC,
+} from 'react';
 import { IndexProps } from '@pages/index';
 import { WholeGridLayout } from '../gridLayout/WholeGridLayout';
 import { MainVisual } from '@organisms/MainVisual';
 import { MainSectionInfo } from '@templates/indexTemplate.core';
 import { AboutSection } from '@organisms/AboutSection';
 import { SkyTheme } from '@molecules/SkyTheme';
+import { SkillsSection } from '@organisms/SkillsSection';
+import {
+  ScrollEffectCallback,
+  useScrollEffect,
+} from '../context/useScrollEffect';
 
 interface IndexTemplateProps extends IndexProps {}
 export const IndexTemplate: VFC<IndexTemplateProps> = (props) => {
+  const [contentsVisiable, setContentsVisiable] = useState(false);
+  const [whileTransition, setWhileTransition] = useState(false);
   const aboutSectionRef = useRef<HTMLElement>(null);
   const skillsSectionRef = useRef<HTMLElement>(null);
   const worksSectionRef = useRef<HTMLElement>(null);
+  const scrollEffect = useScrollEffect();
 
   const mainSectionInfo = useMemo<MainSectionInfo[]>(
     () => [
@@ -21,6 +36,47 @@ export const IndexTemplate: VFC<IndexTemplateProps> = (props) => {
     [],
   );
 
+  const handleVisiableTransitionEnd = useCallback(() => {
+    if (contentsVisiable) {
+      document.body.style.overflow = 'auto';
+    }
+    setWhileTransition(false);
+  }, [contentsVisiable]);
+
+  useEffect(() => {
+    if (!whileTransition && contentsVisiable) {
+      document.body.style.overflow = 'auto';
+    }
+  }, [contentsVisiable, whileTransition]);
+
+  useEffect(() => {
+    const wheelEvent = () => {
+      if (!contentsVisiable) {
+        setWhileTransition(true);
+        setContentsVisiable(true);
+      }
+    };
+    window.addEventListener('wheel', wheelEvent);
+
+    return () => {
+      window.removeEventListener('wheel', wheelEvent);
+    };
+  }, [contentsVisiable]);
+
+  useEffect(() => {
+    const scrollEffectCallback: ScrollEffectCallback = ({ y }) => {
+      if (y !== 0 && !contentsVisiable) {
+        setWhileTransition(true);
+        setContentsVisiable(true);
+      }
+    };
+    scrollEffect.addEffectFunction(scrollEffectCallback);
+
+    return () => {
+      scrollEffect.removeEffectFunction(scrollEffectCallback);
+    };
+  }, [contentsVisiable, scrollEffect]);
+
   return (
     <IndexTemplatePresentational
       {...props}
@@ -28,6 +84,8 @@ export const IndexTemplate: VFC<IndexTemplateProps> = (props) => {
       skillsSectionRef={skillsSectionRef}
       worksSectionRef={worksSectionRef}
       mainSectionInfo={mainSectionInfo}
+      contentsVisiable={contentsVisiable}
+      visiableTransitionEnd={handleVisiableTransitionEnd}
     />
   );
 };
@@ -37,25 +95,42 @@ interface IndexTemplatePresentational extends IndexTemplateProps {
   skillsSectionRef: React.RefObject<HTMLElement>;
   worksSectionRef: React.RefObject<HTMLElement>;
   mainSectionInfo: MainSectionInfo[];
+  contentsVisiable: boolean;
+  visiableTransitionEnd: () => void;
 }
 const IndexTemplatePresentational: VFC<IndexTemplatePresentational> = ({
   aboutSectionRef,
   skillsSectionRef,
   worksSectionRef,
   mainSectionInfo,
+  contentsVisiable,
+  visiableTransitionEnd,
 }) => (
   <WholeGridLayout
     mainVisual={
-      <SkyTheme contents={<MainVisual mainSectionInfo={mainSectionInfo} />} />
+      <header>
+        <SkyTheme
+          contents={
+            <MainVisual
+              mainSectionInfo={mainSectionInfo}
+              contentsVisiable={contentsVisiable}
+            />
+          }
+        />
+      </header>
     }
     contents={
-      <div>
-        <section ref={aboutSectionRef}>
+      <main className="max-w-5xl p-8">
+        <section className="p-8" ref={aboutSectionRef}>
           <AboutSection />
         </section>
-        <section ref={skillsSectionRef}></section>
-        <section ref={worksSectionRef}></section>
-      </div>
+        <section className="p-8" ref={skillsSectionRef}>
+          <SkillsSection />
+        </section>
+        <section className="p-8" ref={worksSectionRef}></section>
+      </main>
     }
+    contentsVisiable={contentsVisiable}
+    visiableTransitionEnd={visiableTransitionEnd}
   />
 );
