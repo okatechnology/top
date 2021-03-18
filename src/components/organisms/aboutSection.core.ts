3ac9376c540853ab301ac4899662269afc987af9
okatechnology <okatechnology2020@gmail.com>
2021-03-18 17:50:55 +0900

    section のテキスト内容変更

--- src/components/organisms/aboutSection.core.ts
+++ src/components/organisms/aboutSection.core.ts
@@ -1,8 +1,8 @@
 export const aboutSectionText: Record<LocaleName, string> = {
   en: `Kaoru Okazoe. Born in 1998
-I love the Internet and Texas Hold'em Poker and eating delicious foods.
+I love the Internet and Texas Hold'em Poker and delicious foods.
 I can develop the Web a little. I'm especially good at the frontend.`,
   ja: `岡添郁。1998年生まれ。
-インターネットとテキサスホールデムポーカーと美味しい食べものを食べることが好き。
+インターネットとポーカーと美味しい食べものが好き。
 ちょっとだけ Web の開発ができます。特にフロントエンドが得意です。`,
 };
