212efeffe5469e5e2d35e8f3feab93b33a8c22f2
okatechnology <okatechnology2020@gmail.com>
2021-03-21 02:02:54 +0900

    fix: button at pagetop's a11y

--- src/components/atoms/ButtonAtPageTop.tsx
+++ src/components/atoms/ButtonAtPageTop.tsx
@@ -31,7 +31,7 @@ const ButtonAtPageTopPresentational: VFC<ButtonAtPageTopPresentationalProps> = (
   handleBlur,
 }) => (
   <button
-    className="w-full rounded-2xl pc:rounded-b-2xl pc:rounded-t-none bg-white dark:bg-gray-900 transition-background-color duration-700 ease-linear pc:hover:shadow-md pc:dark:hover:shadow-md-white focus:outline-none focus:shadow-md focus:ring-2 ring-blue-500"
+    className="overflow-hidden w-full rounded-2xl pc:rounded-b-2xl pc:rounded-t-none pc:hover:shadow-md pc:dark:hover:shadow-md-white focus:outline-none focus:shadow-md focus:ring-2 ring-blue-500 transform pc:hover:translate-y-24 pc:focus:translate-y-24 transition-transform duration-200"
     ref={buttonRef}
     type="button"
     onClick={onClick}
@@ -39,11 +39,13 @@ const ButtonAtPageTopPresentational: VFC<ButtonAtPageTopPresentationalProps> = (
     onMouseOut={handleBlur}
     onBlur={handleBlur}
   >
-    <div className="flex items-center justify-center w-full h-32 text-2xl tracking-wide">
-      {contents}
-    </div>
-    <div className="hidden pc:flex items-center justify-center h-8">
-      <span className="block w-3/4 h-1 rounded-full bg-gray-300 dark:bg-gray-600 transition-background-color duration-700 ease-linear"></span>
+    <div className="bg-white dark:bg-gray-900 transition-background-color duration-700 ease-linear">
+      <div className="flex items-center justify-center w-full h-32 text-2xl tracking-wide">
+        {contents}
+      </div>
+      <div className="hidden pc:flex items-center justify-center h-8">
+        <span className="block w-3/4 h-1 rounded-full bg-gray-300 dark:bg-gray-600 transition-background-color duration-700 ease-linear"></span>
+      </div>
     </div>
   </button>
 );
